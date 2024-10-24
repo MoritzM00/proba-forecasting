240851890ce51151aa2127e24a623620435b486a
Moritz Mistol <moritz.mistol@gmail.com>
2024-10-24 17:41:08 +0200

    add forecast date creation

--- src/probafcst/utils/time.py
+++ src/probafcst/utils/time.py
@@ -1,5 +1,6 @@
 """Utility functions for working with time."""
 
+import numpy as np
 import pandas as pd
 
 
@@ -16,10 +17,35 @@ def get_hours_until_midnight(start: pd.Timestamp | None = None) -> int:
     hours_until_midnight : int
         The number of hours until the next midnight.
     """
+    if start is None:
+        start = pd.Timestamp.now()
     midnight = (start + pd.Timedelta(days=1)).replace(
         hour=0, minute=0, second=0, microsecond=0
     )
     hours_until_midnight = (midnight - start).total_seconds() / 3600
-    # round down
-    hours_until_midnight = int(hours_until_midnight)
-    return hours_until_midnight
+    return int(hours_until_midnight)
+
+
+def get_forecast_dates(start_date: pd.Timestamp):
+    """Get the forecast dates for a submission.
+
+    This function generates the forecast dates for the submission dataframe.
+
+    Parameters
+    ----------
+    start_date : pd.Timestamp
+        The starting date, i.e. the last available timestamp in the data.
+
+    Returns
+    -------
+    tuple of pd.DateTimeIndex, np.ndarray
+        The forecast dates and the relative forecast hours.
+    """
+    relative_hours = np.array([36, 40, 44, 60, 64, 68])
+
+    # relative hours are based on midnight of the start date
+    offset = get_hours_until_midnight(start=start_date)
+
+    relative_hours += offset
+    timestamps = start_date + pd.to_timedelta(relative_hours, unit="h")
+    return timestamps, relative_hours
