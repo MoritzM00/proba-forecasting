7f163a22b0b1c6f632e563b8acbfc19add842ff7
Moritz Mistol <moritz.mistol@gmail.com>
2024-10-25 17:10:18 +0200

    add data functions for bikes and energy

--- src/probafcst/data.py
+++ src/probafcst/data.py
@@ -1,8 +1,11 @@
 """Data module for probafcst package."""
 
+import datetime
 from datetime import date
 
 import pandas as pd
+import requests
+from tqdm import tqdm
 
 
 def get_no2_data(start_date: str = "2020-01-01") -> pd.DataFrame:
@@ -34,3 +37,83 @@ def get_no2_data(start_date: str = "2020-01-01") -> pd.DataFrame:
     # no2 = no2.dropna(axis=0)
 
     return no2
+
+
+def get_bikes_data(start_date: str = "01/01/2019") -> pd.DataFrame:
+    """Get bikes data.
+
+    Parameters
+    ----------
+    start_date : str
+        Start date in format "dd/mm/yyyy".
+
+    Returns
+    -------
+    bikes_df : pd.DataFrame
+        DataFrame with bike data.
+    """
+    dataurl = (
+        "https://www.eco-visio.net/api/aladdin/1.0.0/pbl/publicwebpageplus/data/"
+        f"100126474?idOrganisme=4586&idPdc=100126474&interval=4&flowIds=100126474&debut={start_date}"
+    )
+    response = requests.get(dataurl)
+    rawdata = response.json()
+
+    bikes_df = pd.DataFrame(rawdata, columns=["date", "bike_count"])
+    bikes_df["date"] = pd.to_datetime(bikes_df["date"])
+    bikes_df = bikes_df.set_index("date")
+    bikes_df = bikes_df.astype({"bike_count": float})
+    return bikes_df
+
+
+def get_energy_data(ignore_years: int = 6) -> pd.DataFrame:
+    """Get energy data.
+
+    Parameters
+    ----------
+    ignore_years : int
+        Number of years to ignore.
+
+    Returns
+    -------
+    energy_data : pd.DataFrame
+        DataFrame with bike data.
+    """
+    # get all available time stamps
+    stampsurl = "https://www.smard.de/app/chart_data/410/DE/index_hour.json"
+    response = requests.get(stampsurl)
+
+    # ignore first x years
+    timestamps = list(response.json()["timestamps"])[ignore_years * 52 :]
+
+    col_names = ["date_time", "Netzlast_Gesamt"]
+    energy_data = pd.DataFrame(columns=col_names)
+
+    # loop over all available timestamps
+    for stamp in tqdm(timestamps):
+        dataurl = (
+            "https://www.smard.de/app/chart_data/410/DE/410_DE_hour_"
+            + str(stamp)
+            + ".json"
+        )
+        response = requests.get(dataurl)
+        raw_data = response.json()["series"]
+        for i in range(len(raw_data)):
+            raw_data[i][0] = datetime.fromtimestamp(
+                int(str(raw_data[i][0])[:10])
+            ).strftime("%Y-%m-%d %H:%M:%S")
+
+        energy_data = pd.concat(
+            [energy_data, pd.DataFrame(raw_data, columns=col_names)]
+        )
+
+    energy_data = energy_data.dropna()
+
+    # adjust label
+    energy_data["date_time"] = pd.to_datetime(energy_data.date_time) + pd.DateOffset(
+        hours=1
+    )
+
+    energy_data = energy_data.set_index("date_time")
+
+    return energy_data
