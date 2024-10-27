bb5e79a35cc33d86d2371a16da4f164ccadf2c1d
Moritz Mistol <moritz.mistol@gmail.com>
2024-10-27 19:12:44 +0100

    replace print by logger

--- src/probafcst/utils/check_submission.py
+++ src/probafcst/utils/check_submission.py
@@ -4,6 +4,7 @@ import sys
 from datetime import datetime
 
 import pandas as pd
+from loguru import logger
 
 
 def check_submission(df):
@@ -38,112 +39,112 @@ def check_submission(df):
 
     COLS_QUANTILES = ["q0.025", "q0.25", "q0.5", "q0.75", "q0.975"]
 
-    print("Start checking...")
-    print("---------------------------")
+    logger.info("Start checking...")
+    logger.info("---------------------------")
     col_names = df.columns
 
-    print("Checking the Columns...")
+    logger.info("Checking the Columns...")
     # Check column length
     if len(col_names) != LEN_EXP_COLS:
-        print(
+        logger.info(
             "Dataset contains ", len(col_names), "columns. Required are", LEN_EXP_COLS
         )
-        print("Stopping early...")
+        logger.info("Stopping early...")
         sys.exit()
 
     if set(col_names) != set(EXPECTED_COLS):
-        print("Dataset does not contain the required columns (or more).")
+        logger.info("Dataset does not contain the required columns (or more).")
         missing_cols = set(EXPECTED_COLS) - set(col_names)
-        print("The missing columns are:", missing_cols)
-        print("Stopping early...")
+        logger.info("The missing columns are:", missing_cols)
+        logger.info("Stopping early...")
         sys.exit()
 
     for i, col in enumerate(EXPECTED_COLS):
         if col == col_names[i]:
             continue
         else:
-            print("Columns not in correct order. Order should be:", EXPECTED_COLS)
-            print("Your order is:", col_names.values)
-            print("Stopping early...")
+            logger.info("Columns not in correct order. Order should be:", EXPECTED_COLS)
+            logger.info("Your order is:", col_names.values)
+            logger.info("Stopping early...")
             sys.exit()
 
     # Date Col
-    print("Checking type of columns...")
+    logger.info("Checking type of columns...")
     try:
         df["forecast_date"] = pd.to_datetime(
             df["forecast_date"], format="%Y-%m-%d", errors="raise"
         )
     except (pd.errors.ParserError, ValueError):
-        print("Could not parse Date in format YYYY-MM-DD")
-        print("Stopping early...")
+        logger.info("Could not parse Date in format YYYY-MM-DD")
+        logger.info("Stopping early...")
         sys.exit()
 
     try:
         df["target"] = df["target"].astype("object", errors="raise")
     except ValueError:
-        print("Cannot convert target column to String.")
-        print("Stopping early...")
+        logger.info("Cannot convert target column to String.")
+        logger.info("Stopping early...")
         sys.exit()
 
     try:
         df["horizon"] = df["horizon"].astype("object", errors="raise")
     except ValueError:
-        print("Cannot convert horizon column to String.")
-        print("Stopping early...")
+        logger.info("Cannot convert horizon column to String.")
+        logger.info("Stopping early...")
         sys.exit()
 
     for cq in COLS_QUANTILES:
         if pd.to_numeric(df[cq], errors="coerce").isna().any():
-            print(
-                "----WARNING: Some elements in",
+            logger.warning(
+                "Some elements in",
                 cq,
                 "column are not numeric. This may be fine if you only submit 2 out of 3 targets.",  # noqa: E501
             )
-            print("")
-            # print("Stopping early...")
+            logger.info("")
+            # logger.info("Stopping early...")
             # sys.exit()
 
-    print("Checking if the Dates make sense...")
+    logger.info("Checking if the Dates make sense...")
 
     if len(pd.unique(df["forecast_date"])) > 1:
-        print("forecast_date needs to be the same in all rows.")
-        print("Stopping early...")
+        logger.info("forecast_date needs to be the same in all rows.")
+        logger.info("Stopping early...")
         sys.exit()
 
     if df["forecast_date"][0].date() < datetime.today().date():
-        print("----WARNING: Forecast date should not be in the past.")
-        print("")
+        logger.warning("Forecast date should not be in the past.")
+        logger.info("")
         # warnings.warn("Forecast date should not be in the past.")
 
     if df["forecast_date"][0].weekday() != 2:
-        print("----WARNING: Forecast date should be a Wednesday.")
-        print("")
+        logger.warning("Forecast date should be a Wednesday.")
+        logger.info("")
         # warnings.warn("Forecast date should be a Wednesday")
 
-    print("Checking targets...")
+    logger.info("Checking targets...")
 
     if not df["target"].isin(TARGETS).all():
-        print(f"Target column can only contain {TARGETS}. Check spelling.")
-        print("Stopping early...")
+        logger.info(f"Target column can only contain {TARGETS}. Check spelling.")
+        logger.info("Stopping early...")
         sys.exit()
 
     for target in TARGETS:
         if len(df[df["target"] == target]) != TARGET_LEN[target]:
             if target == "demand":
-                print("Exactly 6 rows need to have target = ", target)
+                logger.info("Exactly 6 rows need to have target = ", target)
             else:
-                print("Exactly 5 rows need to have target =", target)
-            print("Stopping early...")
+                logger.info("Exactly 5 rows need to have target =", target)
+            logger.info("Stopping early...")
             sys.exit()
 
         if (df[df["target"] == target]["horizon"] != TARGET_VALS[target]).any():
-            print(
+            logger.info(
                 "Target",
                 target,
                 "horizons need to be (in this order):",
                 TARGET_VALS[target],
             )
-            print("Stopping early...")
+            logger.info("Stopping early...")
             sys.exit()
 
         if (df[df["target"] == target][COLS_QUANTILES] < TARGET_PLAUS[target][0]).any(
@@ -151,16 +152,16 @@ def check_submission(df):
         ) or (df[df["target"] == target][COLS_QUANTILES] > TARGET_PLAUS[target][1]).any(
             axis=None
         ):
-            print(
-                "----WARNING: Implausible values for",
+            logger.warning(
+                "Implausible values for",
                 target,
                 "detected. You may want to re-check.",
             )
-            print("")
+            logger.info("")
             # warnings.warn("Implausible values for "+str(target)+" detected.
             # You may want to re-check them.")
 
-    print("Checking quantiles...")
+    logger.info("Checking quantiles...")
 
     ALL_NAN_IDX = df[df.isna().any(axis=1)].index
     NAN_TARGET_IDX_LIST = []
@@ -173,7 +174,7 @@ def check_submission(df):
 
         NAN_TARGET_IDX_LIST = df[df["target"] == NAN_TARGET].index
 
-        print(
+        logger.info(
             "Assume that --",
             NAN_TARGET,
             "-- is your NaN-target. Please DOUBLECHECK if this is correct.",
@@ -181,15 +182,15 @@ def check_submission(df):
 
         #        if len(ALL_NAN_IDX) > NAN_TARGET_LENS[NAN_TARGET]:
         if len(ALL_NAN_IDX) > TARGET_LEN[NAN_TARGET]:
-            print(
+            logger.info(
                 "Your dataframe contains more NaNs than entries for target",
                 NAN_TARGET,
                 ".",
             )
-            print("Stopping early...")
+            logger.info("Stopping early...")
             sys.exit()
     else:
-        print("Seems like you submitted all three targets. Good job!")
+        logger.info("Seems like you submitted all three targets. Good job!")
 
     for i, row in df.iterrows():
         if i in NAN_TARGET_IDX_LIST:
@@ -197,18 +198,18 @@ def check_submission(df):
 
         diffs = row[COLS_QUANTILES].diff()
         if diffs[1:].isna().any():
-            print("Something is wrong with your quantiles.")
-            print("Stopping early...")
+            logger.info("Something is wrong with your quantiles.")
+            logger.info("Stopping early...")
             sys.exit()
         diffs.iloc[0] = 0
         if (diffs < 0).any():
-            print(
+            logger.info(
                 "Predictive quantiles in row",
                 i,
                 "are not ordered correctly (need to be non-decreasing)",
             )
-            print("Stopping early...")
+            logger.info("Stopping early...")
             sys.exit()
 
-    print("---------------------------")
-    print("Looks good!")
+    logger.info("---------------------------")
+    logger.info("Looks good!")
