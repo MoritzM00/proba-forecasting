7c0695a6ac74f513b6315c99796fdaf0d6aa456d
Moritz Mistol <moritz.mistol@gmail.com>
2024-11-03 10:16:06 +0100

    update grid search

--- scripts/grid_search.sh
+++ scripts/grid_search.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Array of parameter values
-n_weeks_values=(10 25 50 75 100 125 150)
+n_weeks_values=(10 25 50 75 100 125 150 200 250 300)
 
 # Nested loops to iterate over all combinations
 for n_weeks in "${n_weeks_values[@]}"; do
