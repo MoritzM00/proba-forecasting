cd263c4b349c5414875094a5129ac6311173df11
Moritz Mistol <moritz.mistol@gmail.com>
2024-12-11 10:00:07 +0100

    forecast submission 2024-12-11

--- src/probafcst/utils/__init__.py
+++ src/probafcst/utils/__init__.py
@@ -3,4 +3,11 @@
 from .check_submission import check_submission
 from .create_submission import create_submission
 
-__all__ = ["check_submission", "create_submission", "time", "paths", "sktime"]
+__all__ = [
+    "check_submission",
+    "create_submission",
+    "time",
+    "paths",
+    "sktime",
+    "tabularization",
+]
